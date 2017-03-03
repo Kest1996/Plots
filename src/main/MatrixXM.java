5f0916351718e8c56bc6bfbc882e8296d6214a4d
Kest96 <k.sharipo@mail.ru>
2017-03-03 17:38:18 +0300

    kest

--- src/main/MatrixXM.java
+++ src/main/MatrixXM.java
@@ -1,6 +1,6 @@
 package main;
 
-public class MatrixXM {
+public class MatrixXM implements Matrix {
 
     private Matrix A;
     private Matrix B;
@@ -15,36 +15,51 @@ public class MatrixXM {
         this.kl = kl;
     }
 
-    private double AxB11(){
+    @Override
+    public double M11() {
         return A.M11() * B.M11() - A.Mi11() * B.Mi11() - A.M12() * B.M21() + A.Mi12() * B.Mi21();
     }
-    private double AxB12(){
+
+    @Override
+    public double M12() {
         return A.M11() * B.M12() - A.Mi11() * B.Mi12() + A.M12() * B.M22() - A.Mi12() * B.Mi22();
     }
-    private double AxB21(){
+
+    @Override
+    public double M21() {
         return A.M22() * B.M21() - A.Mi22() * B.Mi21() + A.M21() * B.M11() - A.Mi21() * B.Mi11();
     }
-    private double AxB22(){
+
+    @Override
+    public double M22() {
         return A.M22() * B.M22() - A.Mi22() * B.Mi22() - A.M21() * B.M12() + A.Mi21() * B.Mi12();
     }
-    private double AxBi11(){
+
+    @Override
+    public double Mi11() {
         return A.M11() * B.Mi11() + A.Mi11() * B.M11() - A.M12() * B.Mi21() - A.Mi12() * B.M21();
     }
-    private double AxBi12(){
+
+    @Override
+    public double Mi12() {
         return A.M11() * B.Mi12() + A.Mi11() * B.M12() + A.M12() * B.Mi22() + A.Mi12() * B.M22();
     }
-    private double AxBi21(){
+
+    @Override
+    public double Mi21() {
         return A.M22() * B.Mi21() + A.Mi22() * B.M21() + A.M21() * B.Mi11() + A.Mi21() * B.M11();
     }
-    private double AxBi22(){
+
+    @Override
+    public double Mi22() {
         return A.M22() * B.Mi22() + A.Mi22() * B.M22() - A.M21() * B.Mi12() - A.Mi21() * B.M12();
     }
 
     public double R(){
-        double V = n0 * AxB11() + AxBi21() - nl*(n0*AxBi12()+AxB22()) + kl*(n0*AxB12()-AxBi22());
-        double Z = n0 * AxBi11() - AxB21() + nl*(n0*AxB12()-AxBi22()) + kl*(n0*AxBi12()+AxB22());
-        double X = n0 * AxB11() - AxBi21() - nl*(n0*AxBi12()-AxB22()) + kl*(n0*AxB12()+AxBi22());
-        double Y = n0 * AxBi11() + AxB21() + nl*(n0*AxB12()+AxBi22()) + kl*(n0*AxBi12()-AxB22());
+        double V = n0 * M11() + Mi21() - nl*(n0*Mi12()+M22()) + kl*(n0*M12()-Mi22());
+        double Z = n0 * Mi11() - M21() + nl*(n0*M12()-Mi22()) + kl*(n0*Mi12()+M22());
+        double X = n0 * M11() - Mi21() - nl*(n0*Mi12()-M22()) + kl*(n0*M12()+Mi22());
+        double Y = n0 * Mi11() + M21() + nl*(n0*M12()+Mi22()) + kl*(n0*Mi12()-M22());
 
         return (V*V+Z*Z)/(X*X+Y*Y);
     }
